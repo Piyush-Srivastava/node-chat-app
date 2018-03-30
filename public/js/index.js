8a951d24edab6b3cd7627ede4d72193db0e208f6
Piyush Srivastava <piyush061996@gmail.com>
2018-03-30 18:47:28 +0530

    Emit newMessage on createMessage

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: 'Princess leia',
+        text: 'Help me!'
+    });
+});
+
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message){
+    console.log('Received new message',message);
+});
+
--- server/server.js
+++ server/server.js
@@ -15,10 +15,25 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {//event listener
     console.log("A new user connected.");
 
+    socket.emit('newMessage', {
+        from: 'Obi-wan kenobi',
+        text: 'Hello there',
+        createdAt: 123123
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('Sent new message',message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
     socket.on('disconnect', () => {
         console.log('Disconnected from client');
     });
-});//e
+});
 
 server .listen(port,() => {
     console.log(`Server is up on port ${port}`);
